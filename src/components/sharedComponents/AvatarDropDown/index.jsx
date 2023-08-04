c9f1d422fd48eba3d32bce83665a10bbfafbd395
2000polo <apaul7445@gmail.com>
2023-08-04 22:46:41 +0530

    app bar responsiveness in sm devices is achieved

--- src/components/sharedComponents/AvatarDropDown/index.jsx
+++ src/components/sharedComponents/AvatarDropDown/index.jsx
@@ -10,7 +10,7 @@ const AvatarDropDown = () => {
         <>
             <Stack direction='row' spacing={1} alignItems="center">
                 <Avatar sx={{ bgcolor: blue[900], color: grey[100] }}>A</Avatar>
-                <Stack direction='column'>
+                <Stack sx={{ display: { xs: 'none', sm: 'block' } }} direction='column'>
                     <Typography sx={{margin: 0, lineHeight: 1.5, fontWeight: 300, color: theme.palette.text.primary}} variant="overline" display="block" gutterBottom>
                         Arun Paul
                     </Typography>
