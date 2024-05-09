c92e5a19858dd06a51f586fccc17f189b4fb3786
2000polo <apaul7445@gmail.com>
2024-05-09 14:43:27 +0530

    kanban board modifications and development, Add project card implimnetation in Dashboard, modified routes and theme color

--- src/config/theme/dark.js
+++ src/config/theme/dark.js
@@ -1,18 +1,27 @@
 import { createTheme } from "@mui/material";
-import { blue, grey, pink } from "@mui/material/colors";
+import { blue, green, grey, orange, pink, red, yellow } from "@mui/material/colors";
 
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark', // This specifies the theme mode as dark
     primary: {
-      main: blue[900], // Customize the primary color for dark theme
+      main: '#27272a', // Customize the primary color for dark theme
     },
     secondary: {
       main: pink[300], // Customize the secondary color for dark theme
     },
     background: {
-      default: '#191D29', // Customize the default background color for dark theme
+      // default: '#191D29', // Customize the default background color for dark theme
+      default: '#09090b', // Customize the default background color for dark theme
     },
+    chip:{
+      blue: blue[300],
+      green: green[300],
+      yellow: yellow[700],
+      pink: pink[300],
+      red: red[600],
+      orange: orange[600]
+    }
   },
   // Other dark theme customizations (typography, spacing, etc.) can be added here
 });
--- src/views/app_view/employee_view/dashboard/index.jsx
+++ src/views/app_view/employee_view/dashboard/index.jsx
@@ -2,13 +2,46 @@ import React from 'react';
 import ProjectCard from './components/ProjectCard';
 import { Grid } from '@mui/material';
 import TaskList from './components/task_list';
+import Kanban from '../../../../components/globalComponents/Kanban';
+import AddPorjectCard from './components/AddPorjectCard';
 
 const Dashboard = () => {
+
+    const projectStatus = [
+        {
+            project_name: 'Project Name',
+            desc: 'To be deliverd in 120 days',
+            percentage_completion: 50,
+            project_status: 'In Porgress'
+        },
+        {
+            project_name: 'Project Name',
+            desc: 'To be deliverd in 120 days',
+            percentage_completion: 50,
+            project_status: 'In Porgress'
+        },
+        {
+            project_name: 'Project Name',
+            desc: 'To be deliverd in 120 days',
+            percentage_completion: 50,
+            project_status: 'In Porgress'
+        },
+        // {
+        //     project_name: 'Project Name',
+        //     desc: 'To be deliverd in 120 days',
+        //     percentage_completion: 50,
+        //     project_status: 'In Porgress'
+        // },
+    ];
+
+    console.log("Array lenght", projectStatus?.length);
+    console.log("Array add project card span", 12 - projectStatus?.length * 3);
+
     return (
         <>
         <Grid container spacing={2} direction="column" >
             <Grid container item spacing={2} direction="row" justifyContent= 'space-between'>
-                <Grid item xs={3} >
+                {/* <Grid item xs={3} >
                     <ProjectCard />
                 </Grid>
                 <Grid item xs={3}>
@@ -19,9 +52,26 @@ const Dashboard = () => {
                 </Grid>
                 <Grid item xs={3}>
                     <ProjectCard />
-                </Grid>
+                </Grid> */}
+                {
+                    projectStatus?.map((project) => {
+                        return (
+                            <Grid item xs={3}>
+                                <ProjectCard data={project} />
+                            </Grid>
+                        )
+                    })
+                }
+                {
+                    projectStatus?.length <= 3 ?
+                    <Grid item xs={12 - projectStatus?.length * 3}>
+                        <AddPorjectCard />
+                    </Grid> :
+                    ''
+                }
+                
             </Grid>
-            <Grid container item spacing={2} direction="row" justifyContent= 'space-between' >
+            {/* <Grid container item spacing={2} direction="row" justifyContent= 'space-between' >
                 <Grid item xs={3} >
                     <ProjectCard />
                 </Grid>
@@ -39,9 +89,9 @@ const Dashboard = () => {
                 <Grid item xs={3} >
                     <TaskList />
                 </Grid>
-            </Grid>
+            </Grid> */}
+            {/* <Kanban /> */}
         </Grid>
-        
         </>
     )
 }
