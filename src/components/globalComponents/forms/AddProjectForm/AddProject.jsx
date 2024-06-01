89b4be9daaeb48e5ebd3419fa712532ae51601b6
2000polo <apaul7445@gmail.com>
2024-06-01 20:18:55 +0530

    Add Project form UI modified added more fields

--- src/components/globalComponents/forms/AddProjectForm/AddProject.jsx
+++ src/components/globalComponents/forms/AddProjectForm/AddProject.jsx
@@ -1,7 +1,34 @@
-import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, Stack, TextField, styled, useTheme } from '@mui/material'
+import { Autocomplete, Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, Stack, TextField, styled, useTheme } from '@mui/material'
 import { blue } from '@mui/material/colors';
 import React, { useState } from 'react'
 import ImageUpload from '../../ImageUpload';
+import { DateField } from '@mui/x-date-pickers/DateField';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+
+const FormContainer = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px',
+    padding: '24px 0px',
+    // maxWidth: '400px',
+    margin: 'auto',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    borderRadius: '8px',
+    // backgroundColor: '#fff',
+});
+
+const CustomTextField = styled(TextField)({
+    '& .MuiInputLabel-root': {
+      color: 'blue', // Default label color
+    },
+    '& .MuiInputLabel-root.Mui-focused': {
+      color: '#c7c7c7', // Label color when focused
+    },
+});
+
+
 
 const AddProject = () => {
 
@@ -13,6 +40,15 @@ const AddProject = () => {
         projectManager: '',
         status: '',
     });
+
+    const top100Films = [
+        { title: 'Developer 1', employee_id: 12314 },
+        { title: 'Developer 2', employee_id: 23425 },
+        { title: 'Developer 3', employee_id: 56566 },
+        { title: 'Developer 4', employee_id: 61516 },
+        { title: 'Developer 5', employee_id: 54465 },
+        { title: 'Developer 6', employee_id: 65151 },
+    ]
     
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -24,27 +60,6 @@ const AddProject = () => {
         // Handle form submission logic here
         console.log(formValues);
     };
-
-    const FormContainer = styled(Box)({
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '16px',
-        padding: '24px 0px',
-        // maxWidth: '400px',
-        margin: 'auto',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        borderRadius: '8px',
-        // backgroundColor: '#fff',
-    });
-
-    const CustomTextField = styled(TextField)({
-        '& .MuiInputLabel-root': {
-          color: 'blue', // Default label color
-        },
-        '& .MuiInputLabel-root.Mui-focused': {
-          color: '#c7c7c7', // Label color when focused
-        },
-    });
 
     return (
         <FormContainer>
@@ -78,30 +93,64 @@ const AddProject = () => {
                             required
                         />
                     </Grid>
-                    <Grid item xs={6}>
+                    <Grid item xs={4}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker label="Start Date" />
+                        </LocalizationProvider>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker label="Delivery Date" />
+                        </LocalizationProvider>
+                    </Grid>
+                    <Grid item xs={4}>
                         <CustomTextField
-                            // color={'red'}
                             focused
-                            label="Client Website"
-                            name="website"
-                            value={formValues.website}
-                            onChange={handleInputChange}
+                            label="Status"
+                            name="status"
+                            value={formValues.status}
+                            onChange={(e) => handleInputChange(e)}
                             fullWidth
                             required
-                        />
+                            id={'status'}
+                            select
+                        >
+                            <MenuItem value="Critical">Critical</MenuItem>
+                            <MenuItem value="High">High</MenuItem>
+                            <MenuItem value="Medium">Medium</MenuItem>
+                            <MenuItem value="Low">Low</MenuItem>
+                        </CustomTextField>
                     </Grid>
-                    <Grid item xs={6}>
-                        <CustomTextField
-                            // color={'red'}
-                            focused
-                            label="Client Website"
-                            name="website"
-                            value={formValues.website}
-                            onChange={handleInputChange}
-                            fullWidth
-                            required
-                        />
-                    </Grid>
+                    
+                </Grid>
+                <Grid item xs={6}>
+                    <CustomTextField
+                        // color={'red'}
+                        focused
+                        label="Project Manager"
+                        name="projectManager"
+                        value={formValues.projectManager}
+                        onChange={handleInputChange}
+                        fullWidth
+                        required
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <Autocomplete
+                        multiple
+                        id="tags-outlined"
+                        options={top100Films}
+                        getOptionLabel={(option) => option.title}
+                        defaultValue={[top100Films[3]]}
+                        filterSelectedOptions
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Team Member"
+                                placeholder="Favorites"
+                            />
+                        )}
+                    />
                 </Grid>
             </Grid>
             
@@ -117,29 +166,6 @@ const AddProject = () => {
                 fullWidth
                 required
             />
-            <CustomTextField
-                // color={'red'}
-                focused
-                label="Project Manager"
-                name="projectManager"
-                value={formValues.projectManager}
-                onChange={handleInputChange}
-                fullWidth
-                required
-            />
-            <FormControl fullWidth required>
-                <InputLabel>Status</InputLabel>
-                <Select
-                    label="Status"
-                    name="status"
-                    value={formValues.status}
-                    onChange={handleInputChange}
-                >
-                    <MenuItem value="Not Started">Not Started</MenuItem>
-                    <MenuItem value="In Progress">In Progress</MenuItem>
-                    <MenuItem value="Completed">Completed</MenuItem>
-                </Select>
-            </FormControl>
             
             <Button 
                 component="label"
@@ -149,6 +175,7 @@ const AddProject = () => {
                 // startIcon={<Add />}
                 sx={{bgcolor: blue[900]}}
                 onClick={handleSubmit}
+                size='large'
             >
                 Add Project
             </Button>
